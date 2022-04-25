ee56b3e59440e95309609e1df752442d23c3d7b2
Vladi <vladibuyanov@gmail.com>
2022-04-25 21:46:46 +0200

    Ver. 25/04

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
 
 from config import secret_key
 
@@ -10,10 +12,14 @@ app.config['SECRET_KEY'] = secret_key
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///small-lib.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
+# database
 db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
+
 login_manager = LoginManager(app)
 
-# for blueprint
+# blueprint
 # from admin.admin import admin
 # app.register_blueprint(admin, url_prefix='/admin')
 
@@ -37,6 +43,7 @@ class Books(db.Model):
     author = db.Column(db.String(20))
     year_of_publication = db.Column(db.String(5))
     about = db.Column(db.String)
+    owner = db.Column(db.Integer)
 
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
@@ -47,14 +54,15 @@ def load_user(user_id):
 
 
 # Variables
-user_res = db.session.query(User).all()
 # all_res = db.session.query(User, Books).join(Books, User.id == Books.user_id).all()
+# user_res = db.session.query(User).all()
 # books_res = db.session.query(Books).all()
 
 
 # Main page
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    user_res = db.session.query(User).all()
     books_res = db.session.query(Books).all()
     return render_template('index.html', user_res=user_res, books_res=books_res)
 
@@ -62,8 +70,10 @@ def index():
 # Users dynamic page
 @app.route('/<int:page_id>')
 def user(page_id):
-    books_res = db.session.query(Books).all()
-    return render_template('page_id.html', user_res=user_res[page_id - 1], books_res=books_res)
+    # list of users books
+    user_res = db.session.query(User).all()
+    user_books = Books.query.filter_by(owner=user_res[page_id - 1].id).all()
+    return render_template('page_id.html', user_res=user_res[page_id - 1], user_books=user_books, users=user_res)
 
 
 # Add book page
@@ -77,11 +87,12 @@ def add():
                 author=request.form['author'],
                 year_of_publication=request.form['year_of_publication'],
                 about=request.form['about'],
+                owner=current_user.id,
                 user_id=current_user.id
             )
             db.session.add(book_from_page)
             db.session.commit()
-        except:
+        except Warning:
             db.session.rollback()
             flash('Something is going wrong')
         return redirect(url_for('index'))
@@ -102,7 +113,7 @@ def change_book_info(book_page_id):
         try:
             db.session.commit()
             return redirect(url_for('index'))
-        except:
+        except Warning:
             db.session.rollback()
             return redirect(url_for('index'))
     return render_template('change_book_info.html', book_for_change=book_for_change)
@@ -119,7 +130,7 @@ def give_book(book_page_id):
         try:
             db.session.commit()
             return redirect(url_for('index'))
-        except:
+        except Warning:
             db.session.rollback()
             return redirect(url_for('index'))
     return render_template('give_book.html')
@@ -134,7 +145,7 @@ def delete_book(book_page_id):
         db.session.delete(book_for_delete)
         db.session.commit()
         return redirect(url_for('index'))
-    except:
+    except Warning:
         db.session.rollback()
         return redirect(url_for('index'))
 
@@ -144,16 +155,15 @@ def delete_book(book_page_id):
 def registration():
     if request.method == 'POST':
         if request.form['psw'] == request.form['psw_2']:
+            name = request.form['name']
+            email = request.form['email']
+            psw = generate_password_hash(request.form['psw'], method='sha256')
             try:
-                user_from_page = User(
-                    name=request.form['name'],
-                    email=request.form['email'],
-                    psw=request.form['psw']
-                )
-                db.session.add(user_from_page)
+                new_user = User(name=name, email=email, psw=psw)
+                db.session.add(new_user)
                 db.session.flush()
                 db.session.commit()
-            except:
+            except Warning:
                 db.session.rollback()
                 flash("Something's  going wrong. Please, try again")
                 return render_template('registration.html')
@@ -169,10 +179,12 @@ def registration():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        filter_email = User.query.filter_by(email=request.form['login_email']).all()
-        if len(filter_email) > 0:
-            if request.form['login_psw'] == filter_email[0].psw:
-                login_user(filter_email[0])
+        login_email = request.form['login_email']
+        login_psw = request.form['login_psw']
+        whose_login = User.query.filter_by(email=login_email).all()[0]
+        if whose_login:
+            if check_password_hash(whose_login.psw, login_psw):
+                login_user(whose_login)
                 return redirect(url_for('index'))
             else:
                 flash('Incorrect password')
