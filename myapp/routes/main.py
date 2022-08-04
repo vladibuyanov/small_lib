b51af467b5ddb5c3f7c7823f60f960a36875ab7a
vladibuyanov <vladibuyanov@gmail.com>
2022-08-04 14:25:21 +0200

    Ver. 04/08/2022

--- myapp/routes/main.py
+++ myapp/routes/main.py
@@ -2,13 +2,13 @@ from flask import Blueprint, render_template
 
 from extensions import db
 from models.user import User
-from models.books import Books
+from models.book import Book
 
 main = Blueprint('main', __name__)
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    all_users = db.session.query(User).all()
-    books_res = db.session.query(Books).all()
+    all_users = db.session.query(User).all()[:3]
+    books_res = db.session.query(Book).all()[:3]
     return render_template('index.html', user_res=all_users, books_res=books_res)
