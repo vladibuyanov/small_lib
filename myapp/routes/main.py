9995c5e994ce2d0b8a525a9fe3423c0bcbcede07
vladibuyanov <vladibuyanov@gmail.com>
2022-08-04 17:05:24 +0200

    Ver. 04/08/2022

--- myapp/routes/main.py
+++ myapp/routes/main.py
@@ -7,6 +7,18 @@ main = Blueprint('main', __name__)
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    all_users = db.session.query(User).all()[:3]
-    books_res = db.session.query(Books).all()[:3]
-    return render_template('index.html', user_res=all_users, books_res=books_res)
+    users = db.session.query(User).all()[:3]
+    books = db.session.query(Books).all()[:3]
+    return render_template('index.html', user_res=users, books_res=books)
+
+
+# @main.route('/users', methods=['GET', 'POST'])
+# def index_users():
+#     users = db.session.query(User).all()
+#     return render_template('index.html', user_res=users)
+#
+#
+# @main.route('/books', methods=['GET', 'POST'])
+# def index_books():
+#     books_res = db.session.query(Books).all()
+#     return render_template('index.html', books_res=books_res)
